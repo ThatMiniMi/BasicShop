9e20811b67678b155765d6ed4963d12cd9fdf50d
My <myglimsjo@gmail.com>
2025-05-07 08:22:31 +0200

    Migrations

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea2ae2298e27c4eb78ddd456cc4f658d3de541f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feb71f6c2297ade40a1be24ea41134d25aa443c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
