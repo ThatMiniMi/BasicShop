734d0111885a60f7d97e9633a320570e6fb0eebb
My <myglimsjo@gmail.com>
2025-05-08 14:39:48 +0200

    Ran backend

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+378272f4f7f90753d8f3f95aeb3b9001728bcc25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12ec4df3a36b7d34e6be4b9840aeab3e6670f04d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
