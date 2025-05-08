7a928a7decf501179c150f7fb80c2f2390e51a2e
My <myglimsjo@gmail.com>
2025-05-08 11:33:40 +0200

    Fixed spelling mistake

--- Backend/Models/OrderItem.cs
+++ Backend/Models/OrderItem.cs
@@ -4,7 +4,7 @@ public class OrderItem
     public int Id { get; set; }
     public int ProductId { get; set; }
     public Product? Product { get; set; }
-    public int Guantity { get; set; }
+    public int Quantity { get; set; }
     public decimal PriceAtPurchase { get; set; }
     public int OrderId { get; set; }
     public Order? Order { get; set; }
