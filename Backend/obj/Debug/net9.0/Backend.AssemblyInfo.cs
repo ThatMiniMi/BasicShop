8fd96822fad3280e760c84eaf7a8d2e870736103
My <myglimsjo@gmail.com>
2025-05-15 11:34:35 +0200

    Update

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4269d3d0240a6ea07f9ca60d7983858afa3bc1a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2eccabea0c3a092d605f7150f7866e4800e1a22")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
