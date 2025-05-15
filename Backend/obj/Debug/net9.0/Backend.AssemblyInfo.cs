430f58ccc6a99bb0b7380b8a1b8a9ef4ff0aad23
My <myglimsjo@gmail.com>
2025-05-15 07:51:34 +0200

    Update AdminProductList.jsx

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fb765f32d2e96aef5939903f26c573f57b17b32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b7690fcd80ed1c2d5947b2528667c9a0fa9f621")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
